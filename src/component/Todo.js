de680188165bbf0b9e426ca6db6487bd17a7c428
Luis Filipe Durães Melo <luis.f.duraes.melo@gmail.com>
2021-06-30 15:31:17 +0100

    Added a blank chechbox when the task is not completed yet

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -2,6 +2,7 @@ import { IconButton, ListItem, Typography } from "@material-ui/core";
 import { CheckBox } from "@material-ui/icons";
 import DeleteIcon from "@material-ui/icons/Delete";
 import Grid from "@material-ui/core/Grid";
+import CheckBoxBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
 
 const Todo = ({ todo, completeToggle, removeTodo }) => {
   const checkHandler = () => {
@@ -22,7 +23,11 @@ const Todo = ({ todo, completeToggle, removeTodo }) => {
         spacing={3}
       >
         <Grid item xs={6} sm={3}>
-          <CheckBox checked={todo.completed} onClick={checkHandler} />
+          {!todo.completed ? (
+            <CheckBoxBlankIcon onClick={checkHandler} />
+          ) : (
+            <CheckBox checked={todo.completed} onClick={checkHandler} />
+          )}
         </Grid>
         <Grid item xs={12} sm={6}>
           <Typography
